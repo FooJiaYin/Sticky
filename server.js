2910ca8d441f8339a9bfdc61069afb386710af06
Jia Yin Foo <candy23198@hotmail.com>
2020-06-29 11:17:21 +0800

    add NodeJS

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+//  Import files from source folder( css, js, assets)
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/font', express.static(__dirname + '/font'));
+app.use('/img', express.static(__dirname + '/img'));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/MAINPAGE.html');
+});
